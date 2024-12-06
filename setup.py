595a0af1b6723c8312941e7114cfda4dfa57b17c
sgmurek <shawn.gmurek@gmail.com>
2024-12-05 17:08:03 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version = '0.1.0',
     author = 'wsr7qr',
     author_email = 'wsr7qr@virginia.edu',
-    packages = ['package_directory', 'tests'],
+    packages = ['montecarlo', 'tests'],
     #scripts = ['bin/script1','bin/script2'],
-    url = 'https://github.com/sgmurek/DS5100-wsr7qr/tree/main/final_project',
+    url = 'https://github.com/sgmurek/monte_carlo',
     description = 'A package to create sets of dice, play games with them, and analyze the results.',
     #long_description = open('README.txt').read(),
     install_requires = [
